78d5a323143eb57396493a12ac26adce90f40c91
Netacci <52791481+Netacci@users.noreply.github.com>
2020-06-10 14:14:12 +0100

    homepage and contact page ready, images moved to cloudinary

--- Contact-page/contact.js
+++ Contact-page/contact.js
@@ -65,3 +65,16 @@ nextButton.addEventListener("click", (e) => {
   moveToSlide(track, currentSlide, nextSlide);
   hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
+
+function initMap() {
+  var options = {
+    zoom: 15,
+    center: { lat: 6.4478, lng: 3.4723 },
+  };
+
+  var lekki = { lat: 6.4478, lng: 3.4723 };
+
+  var map = new google.maps.Map(document.querySelector("#map"), options);
+
+  var marker = new google.maps.Marker({ position: lekki, map: map });
+}
